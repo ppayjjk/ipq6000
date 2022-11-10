14424c88da1dbca3b0374c284ec3022dbbfabf60
ppy <92498741+ppayjjk@users.noreply.github.com>
2022-11-10 19:08:03 +0800

    Delete git.sh

--- git.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-git clone -b main --single-branch https://github.com/sdf8057/ipq6000.git openwrt
-cd openwrt
-./scripts/feeds update -a && ./scripts/feeds install -a
-wget https://raw.githubusercontent.com/ppayjjk/ipq6000/main/wifi/.config
-make menuconfig
