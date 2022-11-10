09d866cc766546ed984566b50ae3df15771bd415
ppy <92498741+ppayjjk@users.noreply.github.com>
2022-11-10 15:08:48 +0800

    Update wifi.sh

--- wifi/wifi.sh
+++ wifi/wifi.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+apt update
+apt install make -y
 git clone -b main --single-branch https://github.com/sdf8057/ipq6000.git openwrt
 cd openwrt
 ./scripts/feeds update -a && ./scripts/feeds install -a
